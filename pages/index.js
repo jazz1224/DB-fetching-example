40cf45c068a85c4e044ed2550d99df4714b9eac3
jazz1224 <85280729+jazz1224@users.noreply.github.com>
2022-10-24 21:17:36 +0900

    accompllished

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,62 @@
+import { useState, useEffect } from "react";
+import useSWR from "swr";
+export default function Home(props) {
+  const [sales, setSales] = useState(props.sales);
+  const { data, error } = useSWR(
+    "https://dbfetching-default-rtdb.firebaseio.com/sales.json"
+  );
 
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
 
-export default function Home() {
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          name: data[key].name,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>FAILED TO LOAD.</p>;
+  }
+
+  if (!data && !sales) {
+    return <p>LOADING...</p>;
+  }
   return (
-    <div>
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.name} - {sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
-    </div>
-  )
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://dbfetching-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+  const transformedSales = [];
+
+  for(const key in data) {
+    transformedSales.push({
+      id : key,
+      name : data[key].name,
+      volume : data[key].volume,
+    })
+  }
+
+  return {
+    props : {
+      sales : transformedSales
+    }
+  }
 }
